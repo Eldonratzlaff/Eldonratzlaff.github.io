a3232de440e81a166fd6f06eafed669a0a3ac0b7
Abu bakar <abubakar.abs83@gmail.com>
2023-01-03 15:19:44 +0500

    add hamburger branch by Abu bakar

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const hamburger = document.querySelector('.menu1');
-hamburger.addEventListener('click',display );
-function display() {
-    //alert('It was clicked!');
+// const hamburger = document.querySelector('.menu1');
+// hamburger.addEventListener('click',display );
+// function display() {
+//     //alert('It was clicked!');
 
- let div =document.createElement('div');
- div.className = 'top-name';
-        div.innerHTML = '<p>CreateElement example</p> <h2> hola mundo<h2>';
-        document.body.appendChild(div);
+//  let div =document.createElement('div');
+//  div.className = 'top-name';
+//         div.innerHTML = '<p>CreateElement example</p> <h2> hola mundo<h2>';
+//         document.body.appendChild(div);
 
+// }
+
+const mobile_nav = document.querySelector(".mobile-navbar-btns");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+nav_header.classList.toggle('active')
 }
+
+mobile_nav.addEventListener('click', () => toggleNavbar())
 
