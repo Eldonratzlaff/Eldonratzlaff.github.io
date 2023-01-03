4e64337b8f245162010765057ad3fb0c8965c3f1
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-01-03 12:34:50 -0300

    Errase comented code

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// const hamburger = document.querySelector('.menu1');
-// hamburger.addEventListener('click',display );
-// function display() {
-//     //alert('It was clicked!');
-
-//  let div =document.createElement('div');
-//  div.className = 'top-name';
-//         div.innerHTML = '<p>CreateElement example</p> <h2> hola mundo<h2>';
-//         document.body.appendChild(div);
-
-// }
 
 const mobile_nav = document.querySelector(".mobile-navbar-btns");
 const nav_header = document.querySelector(".header");
