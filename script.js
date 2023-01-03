c1fbc1d11eb4a18d007c8753a7bc9bb82ce508be
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-01-03 14:22:30 -0300

    Link the butons to te correct parts of portfolio

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
 const mobile_nav = document.querySelector(".mobile-navbar-btns");
 const nav_header = document.querySelector(".header");
+const nav_link1 = document.querySelector(".navbar-link1");
+const nav_link2 = document.querySelector(".navbar-link2");
+const nav_link3 = document.querySelector(".navbar-link3");
+const nav_link4 = document.querySelector(".navbar-link4");
 
 const toggleNavbar = () => {
 nav_header.classList.toggle('active')
 }
 
 mobile_nav.addEventListener('click', () => toggleNavbar())
+nav_link1.addEventListener('click', () => toggleNavbar())
+nav_link2.addEventListener('click', () => toggleNavbar())
+nav_link3.addEventListener('click', () => toggleNavbar())
+nav_link4.addEventListener('click', () => toggleNavbar())
 
