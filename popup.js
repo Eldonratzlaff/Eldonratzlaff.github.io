bd3a158f02db99eb8b62a9867481f3f1292574c2
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-01-07 19:53:43 -0300

    Email upercase fix

--- popup.js
+++ popup.js
@@ -216,7 +216,8 @@ form.addEventListener("submit", e =>{
 
 const isValidEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (email.toLowerCase()===email ){ 
+   return re.test(String(email).toLowerCase());}
 }
 
 const validateinputs=() =>{  
@@ -254,9 +255,10 @@ const validateinputs=() =>{
    const succescoment = document.querySelector(".form-coment");
    succescoment.classList.add("succes");
     }
-
-
 }
+//Form validation end
+
+
 
 
 
